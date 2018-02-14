4cbfd55618772e83128d119e49547293546a4882
arubacao <arubacao@gmail.com>
2018-02-14 13:44:07 +0100

    develop first version

---
+++ src/AwsIpRangeMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Arubacao\AwsIpRange;
+
+use Closure;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\IpUtils;
+use GrahamCampbell\GuzzleFactory\GuzzleFactory;
+
+class AwsIpRangeMiddleware
+{
+    const CACHE_KEY = 'arubacao_aws-ip-ranges';
+
+    const URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! IpUtils::checkIp($request->ip(), $this->getAwsIpRanges())) {
+            return response('', 403);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * @return array
+     */
+    private function getAwsIpRanges(): array
+    {
+        return Cache::remember(self::CACHE_KEY, (new \DateTime())->modify('+1 day'), function () {
+            return $this->mergeRanges($this->fetchData());
+        });
+    }
+
+    /**
+     * Fetch ip-ranges from aws
+     *
+     * @return array
+     */
+    private function fetchData(): array
+    {
+        $client = GuzzleFactory::make([]);
+        $response = $client->get(self::URL);
+        $json = $response->getBody()->getContents();
+        return \GuzzleHttp\json_decode($json, true);
+    }
+
+    /**
+     * Merge ipv4 & ipv6
+     *
+     * @param $array
+     * @return array
+     */
+    private function mergeRanges($array): array
+    {
+        $ipRanges = collect($array['prefixes'])->pluck('ip_prefix');
+        $ipV6Ranges = collect($array['ipv6_prefixes'])->pluck('ipv6_prefix');
+        return $ipRanges->merge($ipV6Ranges)->toArray();
+    }
+}
---
+++ tests/AwsIpRangeMiddlewareTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Arubacao\AwsIpRange\Test;
+
+use Illuminate\Support\Facades\Cache;
+use Arubacao\AwsIpRange\AwsIpRangeMiddleware;
+
+class AwsIpRangeMiddlewareTest extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app->router->post('api/sns', ['middleware' => AwsIpRangeMiddleware::class, function () {
+            return 'response';
+        }]);
+    }
+
+    /** @test */
+    public function it_returns_403_non_valid_ip()
+    {
+        $response = $this->post('api/sns');
+        if(is_a($response, 'Illuminate\Foundation\Testing\TestResponse')) {
+            $response->assertStatus(403);
+        } else {
+            $this->seeStatusCode(403);
+        }
+    }
+
+    /** @test */
+    public function it_caches_aws_ip_ranges()
+    {
+        $this->assertFalse(Cache::has(AwsIpRangeMiddleware::CACHE_KEY));
+        $response = $this->post('api/sns');
+        $this->assertTrue(Cache::has(AwsIpRangeMiddleware::CACHE_KEY));
+
+        $cache = Cache::get(AwsIpRangeMiddleware::CACHE_KEY);
+        $download = json_decode(file_get_contents(AwsIpRangeMiddleware::URL), TRUE);
+
+        $this->assertContains($download['prefixes'][0]['ip_prefix'], $cache);
+        $this->assertContains($download['prefixes'][count($download['prefixes']) - 1]['ip_prefix'], $cache);
+        $this->assertContains($download['ipv6_prefixes'][0]['ipv6_prefix'], $cache);
+        $this->assertContains($download['ipv6_prefixes'][count($download['ipv6_prefixes']) - 1]['ipv6_prefix'], $cache);
+    }
+
+    /** @test */
+    public function it_passes_valid_ips()
+    {
+        foreach ($this->getValidIpAwsAddresses() as $ip) {
+            $response = $this->call('POST', 'api/sns', [], [], [], ['REMOTE_ADDR' => $ip]);
+
+            if(is_a($response, 'Illuminate\Foundation\Testing\TestResponse')) {
+                $response->assertStatus(200);
+            } else {
+                $this->seeStatusCode(200);
+            }
+        }
+    }
+
+    /**
+     * An array of valid Aws ip addresses.
+     * @return array
+     */
+    private function getValidIpAwsAddresses()
+    {
+        return [
+            '52.94.196.1',
+            '52.94.196.254',
+            '52.94.198.145',
+            '52.94.198.158',
+            '46.51.192.1',
+            '46.51.207.254',
+            '207.171.167.101',
+            '207.171.167.25',
+            '207.171.167.26',
+            '207.171.172.6',
+            '2a05:d050:c000:0:0:0:0:0',
+            '2a05:d050:c0ff:ffff:ffff:ffff:ffff:ffff',
+            '2a05:d07c:8000:0:0:0:0:0',
+            '2a05:d07c:80ff:ffff:ffff:ffff:ffff:ffff',
+        ];
+    }
+}
